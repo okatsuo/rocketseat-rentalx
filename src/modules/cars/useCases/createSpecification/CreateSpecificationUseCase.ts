ff34c50850b09fed7d4462ba344b6907c5bbe879
okatsuo <rafael.goncanna@gmail.com>
2022-05-22 17:37:14 -0300

    refactor: move specifications to use case

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,10 +1,10 @@
-import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 type IRequest = {
   name: string;
   description: string;
 };
-export class CreateSpecificationService {
+export class CreateSpecificationUseCase {
   constructor(private specificationRepository: ISpecificationsRepository) {}
 
   execute({ name, description }: IRequest): void {
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,19 +1,9 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 export const specificationsRoutes = Router();
 
-const specificationRepository = new SpecificationsRepository();
-
-specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-  return response.status(201).send();
-});
+specificationsRoutes.post("/", (request, response) =>
+  createSpecificationController.handle(request, response)
+);
