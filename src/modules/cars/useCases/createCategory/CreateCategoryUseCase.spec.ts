05cba8de7097c4c98f321312197bf866b02f17c2
okatsuo <rafael.goncanna@gmail.com>
2022-08-25 21:19:46 -0300

    test: ensure CreateCategoryUseCase works correctly

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,49 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateCategoryDTO } from "../../repositories/ICategoriesRepository";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("create category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const category: ICreateCategoryDTO = {
+      name: "valid-name",
+      description: "valid-description",
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    expect(categoriesRepositoryInMemory.categories.length).toBe(1);
+
+    const createdCategory = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(createdCategory).toMatchObject(category);
+    expect(createdCategory).toHaveProperty("id");
+    expect(createdCategory).toHaveProperty("created_at");
+  });
+  it("should not be able to create category with the same name", async () => {
+    const category: ICreateCategoryDTO = {
+      name: "valid-name",
+      description: "valid-description",
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    expect(categoriesRepositoryInMemory.categories.length).toBe(1);
+
+    await expect(
+      createCategoryUseCase.execute(category)
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
