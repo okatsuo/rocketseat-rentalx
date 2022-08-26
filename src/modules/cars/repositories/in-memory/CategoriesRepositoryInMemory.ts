a18e65c63a2fbdcf27c9befd085fd238dbf09c1b
okatsuo <rafael.goncanna@gmail.com>
2022-08-25 21:19:29 -0300

    feat: create a categories in memory repository

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,23 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+export class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async create(data: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+    Object.assign(category, data);
+    this.categories.push(category);
+  }
+
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find((category) => category.name === name);
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+}
